fb8232f6f7787e0a6bcccaebb09b28c7d4fef57e
DanielGutknechtFRA <81569718+DanielGutknechtFRA@users.noreply.github.com>
2021-03-29 15:39:56 +0200

    R code quantile tests
    R code for the simulation of the quantile tests

---
+++ Simulations/Quantile/CQ-SimsFINAL-TEST-2-BW-SELECT.R
@@ -0,0 +1,328 @@
+########################################################
+## Simulation Routine - Conditional Quantile - Test 2 ##
+##         -   Data-Driven Bandwidth  -               ##
+########################################################
+
+rm(list = ls())
+
+
+library(np)
+library(tidyr)
+library(quantreg)
+library(MASS)
+source("Multiplot.R")
+
+
+## Second Test
+LocVBDNStat <- function(Y, qhat, kweights ,taueval) {
+  
+  
+  # nonparametric quantile regression of Y on X
+  Tn.vals <- vector(mode="list",dim(as.matrix(taueval))[1])
+  Var.vals <- vector(mode="list",dim(as.matrix(taueval))[1])
+  epsilonhat <- vector(mode="list",dim(as.matrix(taueval))[1])
+  
+  
+  for (tau in 1:dim(as.matrix(taueval))[1]) {
+    
+    Tn.vals[[tau]] <- sum((((Y - qhat[[tau]]) <=0) - taueval[tau])*kweights)
+    Var.vals[[tau]] <- (3/5)*sum(kweights*((((Y - qhat[[tau]]) <=0) - taueval[tau])^2))
+    epsilonhat[[tau]] <- (Y - qhat[[tau]])
+    
+  }
+  
+  teststat <- max(abs((unlist(Tn.vals))/sqrt(unlist(Var.vals))),na.rm=TRUE)
+  
+  return(list(teststat=teststat, epsilonhat=epsilonhat))
+}
+
+#' compute boostrap statistic
+#' 
+LocWBootStat <- function(Bernl,kweights , taueval) {
+  
+  Tn.vals <- vector(mode="list",dim(as.matrix(taueval))[1])
+  Var.vals <- vector(mode="list",dim(as.matrix(taueval))[1])
+  
+  for (tau in 1:dim(as.matrix(taueval))[1]) {
+    
+    # bootstrap statistic
+    Tn.vals[[tau]] <- sum((Bernl[[tau]] - taueval[tau])*kweights)
+    Var.vals[[tau]] <- mean((Bernl[[tau]] - taueval[tau])^2)*sum(kweights)*(3/5)          
+  } 
+  # bootstrap statistic
+  
+  teststatb <- max(abs(unlist(Tn.vals)/sqrt(unlist(Var.vals))),na.rm=TRUE)
+  
+  return(list(teststat=teststatb))
+}
+
+
+#' perform Volgushev et al. (2013) test 
+#'	
+
+LocVBDNTest <- function(Y, qhat, kweights, taueval, B) {
+    
+  
+  n <- length(Y)
+  # compute test statistic
+  
+  
+  # compute test statistic
+  Tstat <- LocVBDNStat(Y,qhat, kweights, taueval)
+  teststat <- Tstat$teststat
+  epsilonhat <- Tstat$epsilonhat
+  
+  
+  teststatb <- rep(0,B)
+  Bernl <- vector(mode="list",dim(as.matrix(taueval))[1])
+  for (b in 1:B) {
+    for (tau in 1:dim(as.matrix(taueval))[1]) {  
+      Bernl[[tau]] = (runif(length(Y), min = 0, max = 1) <= taueval[[tau]])*1
+    }
+    # bootstrap critical value
+    teststatb[b] <- LocWBootStat(Bernl, kweights, taueval)$teststat
+    
+  }
+  
+  return(list(teststat=teststat, teststatb=teststatb))
+}
+
+
+time=proc.time()
+
+# Set working directory
+
+setwd("C:/Users/gutknech/Downloads/Simulations/Results")
+
+# Set number of Monte Carlo replications
+MC <- 999
+
+# Set the nominal size of both tests
+alpha1 <- 0.05
+alpha2 <- 0.1
+
+etavec <- c(0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9)
+eps <- 0.1
+
+CQ <- 1
+
+# Set the number of bootstrap replications
+Bsize <- 1
+
+
+# Rejection Rate list
+RejRate <- matrix(0,2,2)
+
+
+# Define the grid of quantiles
+tau_grid = c(.3,.5,.7) 
+
+# Set the degree of selection 
+rho <- 0.25
+
+# Set the misspecification parameter:
+gamma1 <- 0.25
+gamma2 <- 1
+
+# Set trimming constant
+trim <- .025
+
+
+
+# Set the sample sizes
+
+SS <- matrix(c(1000, 2000),1,2)
+
+#  BW <- c(1)
+  TestRes <- matrix(0,MC,dim(SS)[2])
+  BootRes <- matrix(0,MC,dim(SS)[2])
+  
+  hgrid<-c(1.25,1.5,1.75,2,2.25,2.5,2.75) 
+  
+
+## Evaluation matrix functions
+
+IndMat <- function(Z) {
+  Z <- as.matrix(Z)
+  indz <- matrix(NA,dim(Z)[1],dim(Z)[1])
+  for (i in 1:dim(Z)[1]) {
+    indz[,i] <- as.numeric(Z<=Z[i])
+  }
+  return(indz)
+}
+
+T1IndMat <- function(Z,Fz) {
+  Z <- as.matrix(Z)
+  indz <- matrix(NA,dim(Z)[1],dim(Z)[1])
+  
+  for (i in 1:dim(Z)[1]) {
+    indz[,i] <- ((Z<=Z[i])-Fz[i])
+  }
+  return(indz)
+}
+
+
+
+Kweights <- function(arg) {
+  
+  arg <- as.matrix(arg)
+  return((3/4)*(1-(arg^2))*(as.numeric(abs(arg)<=1)))
+  
+}
+
+
+
+##########################
+##########################
+### SIMULATION ROUTINE ###
+##########################
+##########################
+
+for (SE in 1:6) {
+  
+  # Set random number seed and get start time
+  
+  set.seed(12345)
+  
+  
+  for (ss in 1:dim(SS)[2]) {
+    
+    
+    Ssize <- SS[ss]
+    
+    for (mc in 1:MC) {
+      ########################
+      # Data Generation      #
+      ########################
+      
+      if ((mc %in% seq(100,100*floor(MC/100),100))==TRUE){
+        cat(mc)
+      } else {
+        cat('.')
+      }
+      
+      
+        
+        if (SE==1) {
+          Z <- rnorm(Ssize,mean=0, sd=1)
+        } else if (SE==2) {
+          Z <- 0.5-rbinom(Ssize, 1, 0.5)
+        } 
+        
+        X <- runif(Ssize, min = 0, max = 1)
+        E <- mvrnorm(Ssize,matrix(c(0,0),2,1),matrix(c(1,rho,rho,1),2,2,byrow=TRUE))
+        S <- ((0.75*(X-0.5)+(0.75*Z))>=(gamma2*E[,2]))
+        
+        
+        Y <- (X-0.5)^3 + (X-0.5)^2 + (X-0.5)+ (gamma1*Z)  + 0.5*E[,1]
+          
+        if (SE==1) {
+          Pz <- pnorm((0.75*(X-0.5)+(0.75*Z)), 0, (gamma2*1))
+        } else if (SE==2) {
+          Pz <- pnorm((0.75*(X-0.5)+(0.75*Z)), 0, (gamma2*1))
+        }    
+        
+      Y <- Y[S]
+      X <- X[S]
+      Pz <- Pz[S]
+
+      filter <- (X<quantile(X,probs=trim))|(X>quantile(X,probs=1-trim))
+      Xtrim <- X[!filter]
+      Ptrim <- Pz[!filter]
+      Ytrim <- Y[!filter]
+      rm(filter)
+      
+      
+      ########################
+      # Conditional Quantile #
+      ########################
+      
+      qhat <- vector(mode="list",dim(as.matrix(tau_grid))[1])
+      n <- dim(as.matrix(Xtrim))[1]
+      for (tau in 1:dim(as.matrix(tau_grid))[1]) {
+        
+        
+        CV_err_h =rep(NA,length(hgrid))
+        for(j in 1:length(hgrid)){
+          h_using = (hgrid[j]*sd(Xtrim)*(n^(-1/5)))
+          CV_err =rep(NA, n)
+          qq <- matrix(0,(n-1),1,byrow=TRUE)
+          for(i in 1:n){
+            X_val = Xtrim[i]
+            Y_val = Ytrim[i]
+            X_tr = Xtrim[-i]
+            Y_tr = Ytrim[-i]
+            Xweight <- Kweights((X_tr-X_val)/h_using)
+            if (colSums(Xweight!=0)<=5) {
+              Xweight <- Kweights((X_tr-X_val)/(h_using+.05))
+            }
+            Xpoly <- (X_tr-X_val)
+            qq[i] <- rq(Y_tr~Xpoly, weights=Xweight, tau=tau_grid[[tau]], ci=FALSE)$coef[1.]
+            CV_err[i] = (Y_val - qq[i])*(tau_grid[[tau]]-((Y_val - qq[i])<=0))
+            
+          }
+          CV_err_h[j] =mean(CV_err)
+        }
+        
+        BW <- (hgrid[which(CV_err_h ==min(CV_err_h))]*sd(Xtrim)*(n^(-1/5)))
+        
+        qq <- matrix(0,length(Xtrim),1,byrow=TRUE)
+        
+        for(i in 1:length(Xtrim)) {
+          
+          Xweight <- Kweights((Xtrim-Xtrim[i])/BW)
+          if (colSums(Xweight!=0)<=5) {
+            Xweight <- Kweights((Xtrim-Xtrim[i])/(BW+.05))
+          }
+          Xpoly <- (Xtrim - Xtrim[i])
+          qq[i] <- rq(Ytrim~cbind(Xpoly,(Xpoly^2),(Xpoly^3)), weights=Xweight, tau=tau_grid[[tau]], ci=FALSE)$coef[1.]
+        }
+        
+        qhat[[tau]] <- qq
+        
+      }
+      kweightfinal <- matrix(NA,1,length(Ptrim))      
+      etaind <- length(etavec)
+      while (etaind > 0) {
+            eta <- etavec[etaind]      
+            hp <- log(n)*(n^(-((1+eps)/(1+eta+eps))))
+            delta <- 1-(hp^(1/(1+eps)))
+            kweight <- Kweights((Ptrim-delta)/hp)
+            if ((sum(kweight)/(n*hp))>=0.1) {
+              kweightfinal <- Kweights((Ptrim-delta)/hp)
+            } else { break }
+            etaind = etaind-1
+      }
+
+      CQ_stat_2 <- LocVBDNTest(Ytrim,qhat, kweightfinal, tau_grid, B=Bsize)
+      TestRes[mc,ss] <- CQ_stat_2$teststat
+      BootRes[mc,ss] <- CQ_stat_2$teststatb
+
+          
+          
+           
+      
+    }
+    
+          cv1 <- quantile(BootRes[,ss], 1-alpha1, na.rm=TRUE)
+          rej1=TestRes[,ss]>cv1
+          cv2 <- quantile(BootRes[,ss], 1-alpha2, na.rm=TRUE)
+          rej2=TestRes[,ss]>cv2
+          RejRate[,ss] <- apply(cbind(rej1,rej2),2,mean)
+
+    
+  }
+  
+  
+  
+  # Pre-allocate empty results table
+  table <- matrix(0,1,2)
+  table <- rbind(matrix(paste("hp-hat",hp,sep=""),1,2),matrix(paste("Gamma1=",gamma1,sep=""),1,2),matrix(paste("SS=",SS,sep=""),1,2),RejRate)
+  # Save csv of results
+  path=paste("MC-Results-CQ-DataDriven-T2-DGP-SE-",SE,"-Gamma1-",gamma1,"-Gamma2-",gamma2,".csv",sep="")
+  write.table(table,path, row.names=FALSE, col.names=FALSE, sep=",")
+  
+}
+
+# Total Running Time:
+cat(paste("Running Time: ",floor(as.matrix(proc.time()-time)[3,]/60),"mins ",round((as.matrix(proc.time()-time)[3,]-60*floor(as.matrix(proc.time()-time)[3,]/60))),"sec",sep=""))
